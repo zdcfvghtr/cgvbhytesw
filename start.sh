80534a0e9a08704fda768bc4e108567f6e9a8040
zdcfvghtr <135286273+zdcfvghtr@users.noreply.github.com>
2023-06-02 00:22:44 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=53c4c60d-298e-4415-9514-811d743a4ec5
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=latestkeralanews.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
